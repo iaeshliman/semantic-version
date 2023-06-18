b69350c60c19ece87c14bf48dd70dedf0a99b673
Isaac Aeshliman <iaeshliman@gmail.com>
2023-06-17 20:44:22 -0500

    wip: test exec package
    Signed-off-by: Isaac Aeshliman <iaeshliman@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const exec = require('@actions/exec')
 
 try {
     console.log('running action')
-
     core.setOutput('version', 'v0.0.0')
+
+    const result = await execute('git', ['tag', '--sort=-v:refname', '-l', 'v*'])
+    console.log(result)
 } catch (error) {
     core.setFailed(error.message)
 }
+
+async function execute(cmd, args) {
+    let output = ''
+    let error = ''
+
+    const options = {
+        listeners: {
+            stdout: (data) => (output += data.toString()),
+            stderr: (data) => (error += data.toString()),
+        },
+    }
+
+    await exec.exec(cmd, args, options)
+
+    if (error !== '') throw new Error(error)
+    return output
+}
+
+/**
+ *
+ *  Command to execute
+ *
+ *  Will return all tags sorted by version and filtered to only include tags matching the pattern `v*`
+ *  `git tag --sort=-v:refname -l v*`
+ *
+ *  Will return the logs of each commit between two (excludes the first commit includes the second commit)
+ *  `git log --oneline commit..commit`
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
