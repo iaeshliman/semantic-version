0eb8561ef33ba1be054633da54e31eb57339c821
Isaac Aeshliman <iaeshliman@gmail.com>
2023-06-17 20:54:29 -0500

    wip: test
    Signed-off-by: Isaac Aeshliman <iaeshliman@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 try {
     console.log('running action')
 
     const result = await execute('git', ['tag', '--sort=-v:refname', '-l', 'v*'])
+    console.log('reached this stage')
     console.log(result)
 
     core.setOutput('version', 'v0.0.0')
@@ -17,10 +17,13 @@ async function execute(cmd, args) {
     let output = ''
     let error = ''
 
-    const options = {
-        listeners: {
-            stdout: (data) => (output += data.toString()),
-            stderr: (data) => (error += data.toString()),
+    const options = {}
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString()
+        },
+        stderr: (data) => {
+            error += data.toString()
         },
     }
 
